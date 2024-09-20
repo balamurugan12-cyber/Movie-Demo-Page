2c6770332808a3c6d1b578f11594fd4a3b9652b0
balamurugan12-cyber <balamuruganm.12523@gmail.com>
2024-09-20 04:16:26 -0700

    adding files

---
+++ java.js
@@ -0,0 +1,11 @@
+var a=document.querySelector(".login")
+var b=document.querySelector("img")
+function inin()
+{
+    a.style.display="inline-block"
+}
+function can()
+{
+    a.style.display="none"
+}
+
